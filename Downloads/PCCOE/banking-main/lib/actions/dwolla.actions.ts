71baf9ad731e8277545e1b7ee3f0e8d80c1144ca
Sahil4505 <sahilpatil4505>
2025-03-02 05:40:35 +0530

    Fixed errors

--- Downloads/PCCOE/banking-main/lib/actions/dwolla.actions.ts
+++ Downloads/PCCOE/banking-main/lib/actions/dwolla.actions.ts
@@ -5,16 +5,7 @@ import { Client } from "dwolla-v2";
 const getEnvironment = (): "production" | "sandbox" => {
   const environment = process.env.DWOLLA_ENV as string;
 
-  switch (environment) {
-    case "sandbox":
-      return "sandbox";
-    case "production":
       return "production";
-    default:
-      throw new Error(
-        "Dwolla environment should either be set to `sandbox` or `production`"
-      );
-  }
 };
 
 const dwollaClient = new Client({
